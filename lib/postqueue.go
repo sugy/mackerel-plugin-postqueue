3eff3d3fc01952bc1784152f10ab225249bf4dd5
sugy <sugy.kz@gmail.com>
2023-06-29 14:48:34 +0900

    Add logic for data entry and metrics counting.

--- lib/postqueue.go
+++ lib/postqueue.go
@@ -1,24 +1,34 @@
 package mppostqueue
 
 import (
+	"bufio"
+	"bytes"
 	"flag"
-	"math/rand"
-	"time"
+	"fmt"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
 
 	mp "github.com/mackerelio/go-mackerel-plugin"
+	log "github.com/sirupsen/logrus"
 	"golang.org/x/text/cases"
 	"golang.org/x/text/language"
 )
 
-// PostqueuePlugin mackerel plugin
+// PostqueuePlugin mackerel plugin for Postfix postqueue metrics
 type PostqueuePlugin struct {
-	Prefix string
+	Prefix          string
+	PostQueuePath   string
+	PostQueueArgs   []string
+	PostQueueOutput string
+	MsgCategories   map[string]*regexp.Regexp
 }
 
 // MetricKeyPrefix interface for PluginWithPrefix
 func (p *PostqueuePlugin) MetricKeyPrefix() string {
 	if p.Prefix == "" {
-		p.Prefix = "postqueue"
+		p.Prefix = "postfix"
 	}
 	return p.Prefix
 }
@@ -26,35 +36,115 @@ func (p *PostqueuePlugin) MetricKeyPrefix() string {
 // GraphDefinition interface for mackerelplugin
 func (p *PostqueuePlugin) GraphDefinition() map[string]mp.Graphs {
 	labelPrefix := cases.Title(language.Und, cases.NoLower).String(p.Prefix)
+	metrics := []mp.Metrics{}
+
+	// Add metrics for Message categories
+	for category := range p.MsgCategories {
+		name := strings.Replace(category, " ", "_", -1)
+		metrics = append(metrics, mp.Metrics{Name: name, Label: category})
+	}
+	metrics = append(metrics, mp.Metrics{Name: "queue", Label: "queue"})
+
+	label := "Postfix postqueue"
+	if len(labelPrefix) > 0 {
+		label = labelPrefix + " postqueue"
+	}
+
 	return map[string]mp.Graphs{
-		"dice": {
-			Label: labelPrefix + " Dice Value",
-			Unit:  "integer",
-			Metrics: []mp.Metrics{
-				{Name: "d6", Label: "Dice(d6)"},
-				{Name: "d20", Label: "Dice(d20)"},
-			},
+		"postqueue": {
+			Label:   label,
+			Unit:    "integer",
+			Metrics: metrics,
 		},
 	}
 }
 
 // FetchMetrics interface for mackerelplugin
 func (p *PostqueuePlugin) FetchMetrics() (map[string]float64, error) {
-	rand.Seed(time.Now().UnixNano())
-	metrics := map[string]float64{
-		"d6":  float64(rand.Intn(6) + 1),
-		"d20": float64(rand.Intn(20) + 1),
+	if p.PostQueueOutput == "" {
+		output, err := p.runPostQueueCommand()
+		if err != nil {
+			return nil, err
+		}
+		log.Debug("FetchMetrics (output): ", fmt.Sprintf("'%v'", output))
+		p.PostQueueOutput = output
 	}
+
+	// Initialize metric map
+	metrics := make(map[string]float64)
+
+	// Read and classify output entries
+	scanner := bufio.NewScanner(strings.NewReader(p.PostQueueOutput))
+	for scanner.Scan() {
+		line := scanner.Text()
+		for category, regex := range p.MsgCategories {
+			if regex.MatchString(line) {
+				log.Debug("FetchMetrics (line): ", fmt.Sprintf("'%v'", line))
+				name := strings.Replace(category, " ", "_", -1)
+				metrics[name] = metrics[name] + 1
+				break
+			}
+		}
+		// line の先頭が 10桁以上の16進数であれば、それはキューIDとみなす
+		if len(line) >= 10 && regexp.MustCompile(`^[0-9A-F]{10,12}[*]{0,1}\s+`).MatchString(line) {
+			metrics["queue"] = metrics["queue"] + 1
+		}
+	}
+	log.Debug("FetchMetrics (metrics): ", fmt.Sprintf("'%v'", metrics))
 	return metrics, nil
+}
+
+// runPostQueueCommand executes the "postqueue -p" command and returns its output
+func (p *PostqueuePlugin) runPostQueueCommand() (string, error) {
+	log.Debug(fmt.Sprintf("command: %v %v", p.PostQueuePath, strings.Join(p.PostQueueArgs, " ")))
+	cmd := exec.Command(p.PostQueuePath, p.PostQueueArgs...)
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	err := cmd.Run()
+	exitCode := cmd.ProcessState.ExitCode()
+
+	if err != nil {
+		return "", fmt.Errorf(fmt.Sprintf("failed to execute postqueue command. exitCode: %d, Stdout: '%s', Stderr: '%s'\n", exitCode, stdout.String(), stderr.String()))
+	}
+	return stdout.String(), err
 }
 
 // Do the plugin
 func Do() {
 	optPrefix := flag.String("metric-key-prefix", "", "Metric key prefix")
+	optDebug := flag.Bool("debug", false, "Debug log level")
+	optPath := flag.String("path", "/usr/sbin/postqueue", "Path to postqueue command")
 	flag.Parse()
 
+	customFmt := new(log.TextFormatter)
+	customFmt.TimestampFormat = "2006-01-02 15:04:05"
+	customFmt.FullTimestamp = true
+	log.SetFormatter(customFmt)
+	log.SetOutput(os.Stdout)
+
+	if *optDebug {
+		log.SetLevel(log.DebugLevel)
+	}
+
 	plugin := mp.NewMackerelPlugin(&PostqueuePlugin{
-		Prefix: *optPrefix,
+		Prefix:        *optPrefix,
+		PostQueuePath: *optPath,
+		PostQueueArgs: []string{"-p"},
+		MsgCategories: map[string]*regexp.Regexp{
+			"Connection timeout":     regexp.MustCompile(`Connection timed out`),
+			"Connection refused":     regexp.MustCompile(`Connection refused`),
+			"Helo command rejected":  regexp.MustCompile(`Helo command rejected: Host not found`),
+			"Host not found":         regexp.MustCompile(`type=MX: Host not found, try again`),
+			"Mailbox full":           regexp.MustCompile(`Mailbox full`),
+			"Network is unreachable": regexp.MustCompile(`Network is unreachable`),
+			"No route to host":       regexp.MustCompile(`No route to host`),
+			"Over quota":             regexp.MustCompile(`The email account that you tried to reach is over quota`),
+			"Relay access denied":    regexp.MustCompile(`Relay access denied`),
+			// Add more log categories with corresponding regular expressions
+		},
 	})
 	plugin.Run()
 }
