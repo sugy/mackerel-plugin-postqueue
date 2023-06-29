675af1fd125d5c07622c1fb2b811591db264c8ae
sugy <sugy.kz@gmail.com>
2023-06-29 14:48:34 +0900

    Add logic for data entry and metrics counting.

--- go.mod
+++ go.mod
@@ -2,9 +2,13 @@ module github.com/sugy/mackerel-plugin-postqueue
 
 go 1.18
 
-require github.com/mackerelio/go-mackerel-plugin v0.1.3
+require (
+	github.com/mackerelio/go-mackerel-plugin v0.1.3
+	github.com/sirupsen/logrus v1.9.3
+	golang.org/x/text v0.3.7
+)
 
 require (
 	github.com/mackerelio/golib v1.2.1 // indirect
-	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 )
--- lib/postqueue_test.go
+++ lib/postqueue_test.go
@@ -1,21 +1,128 @@
 package mppostqueue
 
-import "testing"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"reflect"
+	"regexp"
+	"testing"
 
-func TestFetchMetrics(t *testing.T) {
-	postqueue := &postqueuePlugin{}
+	mp "github.com/mackerelio/go-mackerel-plugin"
+)
 
-	ret, err := postqueue.FetchMetrics()
-
+// Helper function to read a file and return its contents as a string
+func readFile(filePath string) (string, error) {
+	// Open the file
+	file, err := os.Open(filePath)
 	if err != nil {
-		t.Errorf("FetchMetrics returns error")
+		return "", fmt.Errorf("An error occurred while opening the file: %w", err)
+	}
+	defer file.Close()
+
+	// Load the file's contents to a string
+	contents, err := ioutil.ReadAll(file)
+	if err != nil {
+		return "", fmt.Errorf("An error occurred while loading the file: %w", err)
 	}
 
-	if !(1 <= ret["d6"] && ret["d6"] <= 6) {
-		t.Errorf("FetchMetrics doesn't return a value between 1 and 6 for d6 metrics")
-	}
+	return string(contents), nil
+}
 
-	if !(1 <= ret["d20"] && ret["d20"] <= 20) {
-		t.Errorf("FetchMetrics doesn't return a value between 1 and 20 for d20 metrics")
+func TestPostqueuePlugin_GraphDefinition(t *testing.T) {
+	type fields struct {
+		MsgCategories map[string]*regexp.Regexp
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   map[string]mp.Graphs
+	}{
+		{
+			name: "check graph definition",
+			fields: fields{
+				MsgCategories: map[string]*regexp.Regexp{
+					"Foo": regexp.MustCompile(`^Foo\s+\-`),
+				},
+			},
+			want: map[string]mp.Graphs{
+				"postqueue": {
+					Label: "Postfix postqueue",
+					Unit:  "integer",
+					Metrics: []mp.Metrics{
+						{Name: "Foo", Label: "Foo"},
+						{Name: "queue", Label: "queue"},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &PostqueuePlugin{
+				MsgCategories: tt.fields.MsgCategories,
+			}
+			if got := p.GraphDefinition(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("PostqueuePlugin.GraphDefinition() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPostqueuePlugin_FetchMetrics(t *testing.T) {
+	type fields struct {
+		TestdataPath  string
+		MsgCategories map[string]*regexp.Regexp
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    map[string]float64
+		wantErr bool
+	}{
+		{
+			name: "check metrics",
+			fields: fields{
+				TestdataPath: "../testdata/postqueue_output.txt",
+				MsgCategories: map[string]*regexp.Regexp{
+					"Connection timeout":    regexp.MustCompile(`Connection timed out`),
+					"Connection refused":    regexp.MustCompile(`Connection refused`),
+					"Helo command rejected": regexp.MustCompile(`Helo command rejected: Host not found`),
+					"Over quota":            regexp.MustCompile(`The email account that you tried to reach is over quota`),
+				},
+			},
+			want: map[string]float64{
+				"Connection_timeout":    4,
+				"Connection_refused":    1,
+				"Helo_command_rejected": 1,
+				"Over_quota":            1,
+				"queue":                 12,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			// テスト用の出力を読み込む
+			output, err := readFile(tt.fields.TestdataPath)
+			if err != nil {
+				t.Errorf("PostqueuePlugin.FetchMetrics() error = %v", err)
+				return
+			}
+
+			p := &PostqueuePlugin{
+				PostQueueOutput: output,
+				MsgCategories:   tt.fields.MsgCategories,
+			}
+			got, err := p.FetchMetrics()
+			if (err != nil) != tt.wantErr {
+
+				t.Errorf("PostqueuePlugin.FetchMetrics() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("PostqueuePlugin.FetchMetrics() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
