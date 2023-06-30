a0ec6570cb2acfcd17ef1ead7af0f60345e99de7
sugy <sugy.kz@gmail.com>
2023-06-30 11:03:50 +0900

    fix lint

--- lib/version.go
+++ lib/version.go
@@ -17,6 +17,7 @@ func showVersion() {
 		version, commit, UTCToJST(date), runtime.GOOS, runtime.GOARCH, runtime.Version())
 }
 
+// UTCToJST converts UTC to JST
 func UTCToJST(utc string) string {
 	d, err := time.Parse("2006-01-02T15:04:05Z", utc) //Parse RFC3339
 	if err != nil {
